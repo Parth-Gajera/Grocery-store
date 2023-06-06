75593e7cbe2da2469323cc505c2ec8bd10d28835
Parth-Gajera <126321337+Parth-Gajera@users.noreply.github.com>
2023-06-06 09:43:38 +0530

    Update FAQ.js

--- src/component/FAQ.js
+++ src/component/FAQ.js
@@ -7,18 +7,17 @@ function FAQ() {
 
         <div><h1 style={{ textAlign: 'center' }}>FAQ</h1>
 
-            <div className='row container r4'>
-
-                <div className='col-lg-7'>
-                    <FAQ1 />
+            <div className='container r4'>
+                <div className='row'>
+                    <div className='col-lg-7'>
+                        <FAQ1 />
+                    </div>
+                    <div className='col-lg-5' id='part'>
+                        <iframe width="150" height="150px" src="https://www.youtube.com/embed/FvFsMjRfvyI" title="Invernadero de Almería con tomates cherrys rama Golán ." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen id='vid' ></iframe>
+                    </div>
                 </div>
-
-                <div className='col-lg-5' id='part'>
-                    <iframe width="150" height="150px" src="https://www.youtube.com/embed/FvFsMjRfvyI" title="Invernadero de Almería con tomates cherrys rama Golán ." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen id='vid' ></iframe>
-                </div>
-
             </div>
-            
+
         </div >
 
 
