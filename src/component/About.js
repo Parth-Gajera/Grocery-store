67e23d320054d82da26ef369c62bff1674f6d3bf
Parth-Gajera <126321337+Parth-Gajera@users.noreply.github.com>
2023-06-06 09:43:11 +0530

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
+
 
 
 import Nav from './Nav';
@@ -20,35 +19,34 @@ import quote2 from '../img/quote2.png';
 function About() {
 	return (
 		<div>
-			<Nav/>
-
+			<Nav />
 			<div>
-				<div className='row  container service'>
+				<div className='container' style={{ padding: '2rem' }}>
 					<h3 style={{ textAlign: 'center' }}>WHY<span style={{ color: '#55b142' }}> GRO4U</span>?</h3>
-					<h6 style={{ textAlign: 'center' }}>What We Serve</h6>
-				
+					<h6 style={{ textAlign: 'center', 'padding-bottom': '10px' }}>What We Serve</h6>
+					<div className='row service'>
 						<div className='col-lg-3 block' >
 							<span className='icon4'><img src={fastdelivery} width='100%'></img></span>
-							<h5>FREE FAST DELIVERY</h5>
+							<h5 className='ser-head'>FREE FAST DELIVERY</h5>
 							<p>Get free delivery faster ever enjoy the freshness of the grocery</p>
 						</div>
 
 						<div className='col-lg-3 block'>
 							<span className='icon4'><img src={returninstant} width='100%'></img></span>
-							<h5>INSTANT RETURN</h5>
+							<h5 className='ser-head'>INSTANT RETURN</h5>
 							<p> If you got rotten items  return immediately to us  </p>
 						</div>
-						
+
 						<div className='col-lg-3 block'>
 							<span className='icon4'><img src={payment} width='100%'></img></span>
-							<h5>SECURE PAYMENT</h5>
+							<h5 className='ser-head'>SECURE PAYMENT</h5>
 							<p> 100% Secure payment we accept any type of payment</p>
 						</div>
-					
+					</div>
 				</div>
 
 				<div className='container '>
-					<h3 style={{ textAlign: 'center' }}>HAPPY <span style={{ color: '#55b142' }}> CUSTMOERS</span></h3>
+					<h3 style={{ textAlign: 'center', 'padding-top': "2rem" }}>HAPPY <span style={{ color: '#55b142' }}> CUSTMOERS</span></h3>
 					<h6 style={{ textAlign: 'center' }}> Oue Custmoer Say About Us</h6>
 					<span className='quote'><img src={quote} width='100%'></img></span>
 					<Carousel1 />
