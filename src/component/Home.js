2aadc05775af17245733af0ecd0fad196e00d5f7
Parth-Gajera <126321337+Parth-Gajera@users.noreply.github.com>
2023-06-06 09:44:28 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
+
 
 
 import Nav from './Nav';
 import Footer from './Footer';
-
 import dboy1 from '../img/dboy1.png';
 
 
@@ -15,23 +13,22 @@ import dboy1 from '../img/dboy1.png';
 function Home() {
   return (
     <div>
-        <Nav />
+
+      <Nav />
       <div className='container-fluid '>
         <div className='container r3' >
-
           <div className='col1' >
             <h1 id='head2'>Bring The <span style={{ color: '#55b142' }}>Store</span><br />To Your <span style={{ color: '#55b142' }}>Door</span></h1>
             <h6>Get the best quailty and the most delicious grocery food <br /> in the world you can get them on your door<br />by useing our website </h6>
             <button id='btn3'> <a href="/Groceries" className='link2'>Shop Now</a></button>
           </div>
-
           <div className='col2'>
             <img src={dboy1} width='100%' style={{ border: '#55b142' }}></img>
           </div>
 
         </div>
       </div>
-        <Footer />
+      <Footer />
     </div>
   )
 }
