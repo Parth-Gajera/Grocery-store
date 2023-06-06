8bc8dc0f8db105d7a7202b021bae7d958f4c783d
Parth-Gajera <126321337+Parth-Gajera@users.noreply.github.com>
2023-06-06 09:45:07 +0530

    Update Nav.js

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -2,98 +2,95 @@ import React from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
-// import Toggle from './Toggle'
 
 
-import { FaSearch } from "react-icons/fa";
-import { GoLocation } from "react-icons/go";
-// import search from '../img/s1.png';
-import { MdOutlineCall } from "react-icons/md";
+
+
 import imgLogo from '../img/logo.png';
 import user from '../img/u1.png';
 import cart from '../img/cart.png';
 import wish from '../img/wish.png';
 
-import { Link } from 'react-router-dom';
+
 
 function Nav() {
 
 	return (
 		<div>
 
-			<nav class="navbar navbar-expand-lg bg-body-tertiary header">
+			<nav className="navbar navbar-expand-lg  header">
 
-				<div class="container-fluid">
+				<div className="container-fluid">
 					<div className='container'>
 
 						<div className='top-header'>
 
 							<div className='top'>
-								<a class="navbar-brand logo" href="#"><img src={imgLogo} width="100%" alt="" /></a>
-								<button class="navbar-toggler top2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-									<span class="navbar-toggler-icon"></span>
+								<a className="navbar-brand logo" href="#"><img src={imgLogo} width="100%" alt="" /></a>
+								<button className="navbar-toggler top2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+									<span className="navbar-toggler-icon"></span>
 								</button>
 							</div>
 
-							<div className='sea'>
-								<button href='#' className='btn'><span className='txt'>All Categories</span></button>
-								<input className='input5' type="search" placeholder="Search product... " aria-label="Search" />
-								<a href='#' className='put'>< FaSearch /></a>
+							<div className='search'>
+								<a href='#Home' className='sea-btn'>All Categories</a>
+								<input className='sea-in' type="search" placeholder="Search product... " aria-label="Search" />
+
 							</div>
-							
+
 						</div>
 
-						<div class="collapse navbar-collapse  " id="navbarTogglerDemo02">
+						<div className="collapse navbar-collapse" id="navbarTogglerDemo02">
 
-							<form class="d-flex" role="search">
-								<input class="form-control me-2" type="location" placeholder="Order Tracking ..." aria-label="Search" />
-								<a href='#' className='put'><GoLocation /></a>
-							</form>
+							<div className="track" role="search">
 
-							<ul class="navbar-nav me-auto mb-2 mb-lg-0 bot-header">
-								<li class="nav-item">
-									<a class="nav-link active tab" aria-current="page" href="/">Home</a>
+								<input className="track-in" type="search" placeholder="Order Tracking ..." aria-label="Search" />
+							</div>
+
+							<ul className="navbar-nav me-auto mb-2 mb-lg-0 bot-header">
+								<li className="nav-item">
+									<a className="nav-link  tab" aria-current="page" href="/">Home</a>
 								</li>
-								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle active tab" href="/Groceries" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								<li className="nav-item dropdown">
+									<a className="nav-link dropdown-toggle  tab" href="/Groceries" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 										Groceries
 									</a>
-									<ul class="dropdown-menu dropdown-menu-dark">
-										<li><a class="dropdown-item" href='/Groceries#Vegetables'>Vegetables</a></li>
-										<li><a class="dropdown-item" href='/Groceries#Fruits' >Fruits</a></li>
-										<li><a class="dropdown-item" href='/Groceries#Dryfruits'>Dry Fruits</a></li>
-										<li><a class="dropdown-item" href='/Groceries#Tea'>Tea & Coffee</a></li>
+									<ul className="dropdown-menu dropdown-menu-dark">
+										<li><a className="dropdown-item" href='/Groceries#Vegetables'>Vegetables</a></li>
+										<li><a className="dropdown-item" href='/Groceries#Fruits' >Fruits</a></li>
+										<li><a className="dropdown-item" href='/Groceries#Dryfruits'>Dry Fruits</a></li>
+										<li><a className="dropdown-item" href='/Groceries#Tea'>Tea & Coffee</a></li>
 									</ul>
 								</li>
-								<li class="nav-item ">
-									<a class="nav-link active tab" href="/Offer">Offer</a>
+								<li className="nav-item ">
+									<a className="nav-link  tab" href="/Offer">Offer</a>
 								</li>
-								<li class="nav-item">
-									<a class="nav-link active tab" href='/About'>About us</a>
+								<li className="nav-item">
+									<a className="nav-link tab" href='/About'>About us</a>
 								</li>
 							</ul>
 
 							<ul className='ricon'>
-								<li class="nav-item">
+								<li className="nav-item">
 									<a href='#' className='icon nav-link'>
 										<img src={user} width='100%' />
 										<sapn className="name" >Profile</sapn>
 									</a>
 								</li>
-								<li class="nav-item">
+								<li className="nav-item">
 									<a href='#' className='icon nav-link'>
 										<img src={cart} width='100%' />
 										<sapn className="name" >Cart</sapn>
 									</a>
 								</li>
-								<li class="nav-item">
+								<li className="nav-item">
 									<a href='#' className='icon nav-link' >
 										<img src={wish} width='100%' />
 										<sapn className="name" >Wish</sapn>
 									</a>
 								</li>
 							</ul>
-							
+
 						</div>
 
 					</div >
